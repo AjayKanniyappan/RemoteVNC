a17649895a7cd53eb2bcb9c860f4bf79470e9478
ajaykanniyappan <ajaykanniyappan@gmail.com>
2022-11-24 00:11:41 +0530

    Tailwindcss Configurations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className={styles.center}>
       <h1>Hello World!</h1>
+      <h2 className="text-slate-400 hover:text-sky-400 text-3xl font-bold text-center">Tailwind</h2>
     </div>
   );
 }
