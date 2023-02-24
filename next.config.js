2a49664be9fd920d3f5c45aeca0a3c5cbd4579f4
ajaykanniyappan <ajaykanniyappan@gmail.com>
2023-02-24 12:20:52 +0530

    Starter

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development', // 👈 DISABLING PWA IN DEVELOPMENT MODE
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+});
+
+const nextConfig = withPWA({
+  reactStrictMode: false, // 👈 DISABLING THIS TO AVOID DOUBLE RENDER
+});
+
+module.exports = nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="text-3xl font-bold">Hello</div>
+    </main>
+  );
+}
